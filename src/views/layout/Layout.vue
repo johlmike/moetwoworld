fa55aa92fd57d525218b576c9c89e588281e9ca4
Johlmike <johlmike@gmail.com>
2020-08-06 10:42:58 +0800

    在使用者頁面 layout 讀取商品列表和購物車列表

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -10,6 +10,49 @@ import Navbar from '@/components/Navbar.vue';
 
 export default {
   components: { Navbar },
+  data() {
+    return {
+      baseUrl: process.env.VUE_APP_BASEURL,
+      uuid: process.env.VUE_APP_UUID,
+      products: [],
+      page: 1,
+      cart: [],
+    };
+  },
+  methods: {
+    getProducts() {
+      const loader = this.$loading.show();
+      const url = `${this.baseUrl}${this.uuid}/ec/products?page=${this.page}&paged=9`;
+      this.axios
+        .get(url)
+        .then((res) => {
+          loader.hide();
+          this.products = [...res.data.data];
+        })
+        .catch((res) => {
+          console.log(res.data);
+        });
+    },
+    getCart() {
+      const loader = this.$loading.show();
+      const url = `${this.baseUrl}${this.uuid}/ec/shopping`;
+      this.axios
+        .get(url)
+        .then((res) => {
+          loader.hide();
+          this.cart = [...res.data.data];
+        })
+        .catch((err) => {
+          loader.hide();
+          console.log(err.response);
+        });
+    },
+  },
+  created() {
+    // 由 layout 控制商品列表和購物車，減少 Ajax 讀取次數
+    this.getProducts();
+    this.getCart();
+  },
 };
 </script>
 
